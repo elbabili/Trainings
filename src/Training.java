b2038b9f056f55058a7e7e2e0baf8b7c1351c6de
elbabili <elbabilimohamed@gmail.com>
2022-02-21 08:44:49 +0100

    display complete trainings

--- src/Training.java
+++ src/Training.java
@@ -3,14 +3,21 @@ public class Training {
 
 	public static void main(String[] args) {
 		System.out.println("Bonjour et bienvenue dans mon application FullTrainings");
-		System.out.println("Nous allons vous proposer une liste de formation actuellement disponible");
+		System.out.println("Nous allons vous proposer une liste de formation actuellement disponible \n \n");
 		
-		String trainings[]  = {"Java","Php","C#","React","Angular"};
-		System.out.println("-------------------------");
+		String trainings[][]  = {
+				{"Java          ", 	"20" , "Java SE 8 : Syntaxe & Poo            " 	, "3000"},
+				{"Java avancé   ", 	"20" , "Exceptions, fichiers, Jdbc, thread..."  , "5000"},
+				{"Spring        ",	"20" , "Spring Core/Mvc/Security             "	, "5000"},
+				{"Php frameworks",	"15" , "Symphony                             "	, "2500"},
+				{"C#            ",  "20" , "DotNet Core                          "  , "5000"},
+								};
 		
 		for(int i=0 ; i<trainings.length ; i++) {
-			System.out.print(trainings[i] + " ");
+			for(int j=0 ; j<trainings[i].length; j++) {				
+				System.out.print(trainings[i][j] + " | ");
+			}
+			System.out.println();
 		}
-		System.out.println("\n-------------------------");
 	}
 }
